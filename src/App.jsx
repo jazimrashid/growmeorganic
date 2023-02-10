f8882d3700138d4c76ad6387eb07e9d5b44b08d0
jazimrashid <jaziml27@gmail.com>
2023-02-10 19:23:02 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import Form from "./Components/Form/Form";
 import Datalist from "./Components/Datalist/Datalist";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes  } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      
       <Routes>
         <Route exact path="/" element={<Form />} />
 
         <Route exact path="/Home" element={<Datalist />} />
       </Routes>
+      
     </div>
   );
 }
